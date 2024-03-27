9663be5a2c37af9a87a34617901d4015ddca9813
HasanliHM <hacaga.hasanli@kapitalbank.az>
2024-03-27 22:49:24 +0400

    toggle button component and testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [buttonColor, setButtonColor] = useState('red');
+  const nextColor = buttonColor === 'red' ? 'blue' : 'red';
+
+  const onButtonClick = () => setButtonColor(nextColor);
+
   return (
     <div>
-      <button className='red'>Change to blue</button>
+      <button
+        className={buttonColor}
+        onClick={onButtonClick}
+      >{`Change to ${nextColor}`}</button>
+      <br />
+      <input
+        type='checkbox'
+        defaultChecked={false}
+        id='disabled-button-checkbox'
+      />
+      <label htmlFor='disable-button-checkbox'>Disable button</label>
     </div>
   );
 }
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,14 +1,46 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 
 import App from './App';
 
-test('should button starts with correct color', () => {
+test('should button starts with correct label and color', () => {
+  //render the App
   render(<App />);
 
+  //find the button
   const buttonElement = screen.getByRole('button', { name: /blue/i });
+
+  //check the initial color
   expect(buttonElement).toHaveClass('red');
 });
 
-test('should button has correct color after click', () => {});
+test('should button has correct label and color after click', () => {
+  //render the App
+  render(<App />);
 
-test('should button has correct text after click', () => {});
+  //find the button
+  const buttonElement = screen.getByRole('button', { name: /blue/i });
+
+  //check the initial color
+  expect(buttonElement).toHaveClass('red');
+
+  //click the button
+  fireEvent.click(buttonElement);
+
+  //check the button text
+  expect(buttonElement).toHaveTextContent(/red/i);
+
+  //check the button color
+  expect(buttonElement).toHaveClass('blue');
+});
+
+test('checkbox flow', () => {
+  render(<App />);
+
+  //find elements
+  const buttonElement = screen.getByRole('button', { name: /blue/i });
+  const checkboxElement = screen.getByRole('checkbox', { name: /disable button/i });
+
+  //check initial conditions
+  expect(buttonElement).toBeEnabled()
+  expect(checkboxElement).not.toBeChecked()
+});
